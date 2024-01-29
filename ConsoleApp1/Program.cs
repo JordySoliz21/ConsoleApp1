fdd18438a903c486b61b223af7a72e2cf9031441
Sistemas2-3 <Sistemas2-3@DESKTOP-2V34ILT>
2024-01-29 16:27:42 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.Title = " El mayor de dos numeros";
+    Int32 x, y;
+
+Console.WriteLine("Digitar el primer número del 1 al 1000");
+    x = Int32.Parse(Console.ReadLine());
+
+ Console.WriteLine("Digitar el segundo número del 1 al 1000");
+    y = Int32.Parse(Console.ReadLine());
+
+    if (x > y)
+    {
+        Console.WriteLine("\nEl numero {0} es mayor que {1}", x, y); // v
+    }
+    if (x == y)
+    {
+    Console.WriteLine("\nEl numero {0} es igual a {1}", x, y);
+    }
+    else
+    {
+        Console.WriteLine("\nEl numero {0} es mayor que {1}", y, x); // f
+    }
+
+    Console.WriteLine("\n\n");
+    Console.WriteLine("\n----->Fin del programa");
+    Console.ReadKey();
+
---
+++ NumeroParImpar/Program.cs
@@ -0,0 +1,34 @@
+﻿int Numero;
+Console.Title = "VERIFICANDO NUMEROS PARES";
+Console.Write("\nIngrese un numero:");
+Numero = int.Parse(Console.ReadLine());
+
+if (Numero < 0)
+{
+    Console.Write("\n\tNumero Negativo...Ingrese un numero positivo");
+}
+
+if (Numero == 0)
+{
+    Console.Write("\n\tLA DIVISIÓN DE CERO NO EXISTE");
+}
+
+else if (Numero % 2 == 0)
+{
+    Console.Write("\n\tEl numero (" + Numero + ") es par.");
+    if (Numero >= 10 && Numero <= 100)
+    {
+        Console.Write("\n\tEl numero (" + Numero + ") se encuentra en el rango [10-100]");
+    }
+    else
+    {
+        Console.Write("\n\tEl numero (" + Numero + ") NO esta en el rango [10-00]");
+    } 
+}
+else
+{
+    Console.Write("\n\tEl numero ingresado es impar...");
+}
+    Console.ReadKey();
+
+
